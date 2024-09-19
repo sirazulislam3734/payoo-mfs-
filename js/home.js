8061bcd7c14480b3e680f8b59a4896c1044a8a9c
Riaz Uddin <sirazulislam3734@gmail.com>
2024-09-19 16:53:57 +0600

    added the add money and event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
-console.log('hello');
+// add money to the account
+
+// step: 1 add an event handler to the add moneybtn iside the form
+document.getElementById('add-money-btn').addEventListener('click', function(event){
+    // prevent page reload after form submit
+    event.preventDefault();
+
+    // step-2 get money to be added to the account
+    let inputAddMoney = document.getElementById('input-add-money').value ;
+    console.log(inputAddMoney);
+    let inputPin = document.getElementById('input-pin').value ;
+    console.log(inputPin);
+         })
